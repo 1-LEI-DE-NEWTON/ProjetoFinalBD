651436d3a61c5fc00bd08ee597f38c09935b29b3
Victor Guilherme <TKFireZ02@outlook.com>
2024-09-12 20:36:48 -0300

    elaboração de bandeiracartaoDAO e reservaDAO

---
+++ DAO/BandeiraCartaoDAO.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjetoFinalBD.Models;
+
+namespace ProjetoFinalBD.DAO
+{
+    public class BandeiraCartaoDAO : DAOBase
+    {
+        public BandeiraCartaoDAO(string connectionString) : base(connectionString) { }
+
+        public void Insert(BandeiraCartao bandeiraCartao)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO BandeiraCartao (Descricao) VALUES (@Descricao)";
+                    command.Parameters.AddWithValue("Descricao", bandeiraCartao.Descricao);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public BandeiraCartao GetById(int id)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT Id, Descricao FROM BandeiraCartao WHERE Id = @Id";
+                    command.Parameters.AddWithValue("Id", id);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new BandeiraCartao
+                            {
+                                Id = reader.GetInt32(0),
+                                Descricao = reader.GetString(1)
+                            };
+                        }
+                    }
+                }
+                return null;
+            }
+        }
+
+        public void Update(BandeiraCartao bandeiraCartao)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "UPDATE BandeiraCartao SET Descricao = @Descricao WHERE Id = @Id";
+                    command.Parameters.AddWithValue("Descricao", bandeiraCartao.Descricao);
+                    command.Parameters.AddWithValue("Id", bandeiraCartao.Id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "DELETE FROM BandeiraCartao WHERE Id = @Id";
+                    command.Parameters.AddWithValue("Id", id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ DAO/ReservaDAO.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjetoFinalBD.Models;
+
+namespace ProjetoFinalBD.DAO
+{
+    public class ReservaDAO : DAOBase
+    {
+        public ReservaDAO(string connectionString) : base(connectionString) { }
+
+        public void Insert(Reserva reserva)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                
+                var transaction = connection.BeginTransaction();
+                
+                try
+                {
+                    //Inserir reserva
+
+                    string query = "INSERT INTO Reserva (Id, Saldo, Taxa, ReservaCol, ContaId) " +
+                            "VALUES (@Id, @Saldo, @Taxa, @ReservaCol, @ContaId)";
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = query;
+                        command.Parameters.AddWithValue("Id", reserva.Id);
+                        command.Parameters.AddWithValue("Saldo", reserva.Saldo);
+                        command.Parameters.AddWithValue("Taxa", reserva.Taxa);
+                        command.Parameters.AddWithValue("ReservaCol", reserva.ReservaCol);
+                        command.Parameters.AddWithValue("ContaId", reserva.Conta.Id);
+
+                        command.ExecuteNonQuery();
+                    }
+
+                    //Inserir movimentações
+                    foreach (var movimentacaoReserva in reserva.MovimentacoesReserva)
+                    {
+                        //Criar string para query                        
+                        string insertMovimentacaoQuery = "INSERT INTO movimentacaoReserva " +
+                            "(Id, DataMovimentacao, TipoMovimentacao, Valor, ReservaId) VALUES " +
+                            "(@Id, @DataMovimentacao, @TipoMovimentacao, @Valor, @ReservaId)";
+
+                        using (var command = connection.CreateCommand())
+                        {
+                            command.CommandText = insertMovimentacaoQuery;
+                            command.Parameters.AddWithValue("Id", movimentacaoReserva.Id);
+                            command.Parameters.AddWithValue("DataMovimentacao", movimentacaoReserva.DataMovimentacao);
+                            command.Parameters.AddWithValue("TipoMovimentacao", movimentacaoReserva.TipoMovimentacao);
+                            command.Parameters.AddWithValue("Valor", movimentacaoReserva.Valor);
+                            command.Parameters.AddWithValue("ReservaId", reserva.Id);
+
+                            command.ExecuteNonQuery();
+                        }
+                    }
+
+                    transaction.Commit();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+    }
+}
