bac13c0a809c5fa96f134cb6548bfd8f9a8ff9fe
Victor Guilherme <TKFireZ02@outlook.com>
2024-09-15 16:21:21 -0300

    randomização de testes

--- Testes/TesteContaDAO.cs
+++ Testes/TesteContaDAO.cs
@@ -27,106 +27,147 @@ namespace ProjetoFinalBD.Testes
             cartaoCreditoDAO = new CartaoCreditoDAO(connectionString);
         }  
         
-public Cliente GerarClienteAleatorio()
-    {
-        // Listas de valores predefinidos
-        List<string> nomes = new List<string> { "João", "Maria", "Pedro", 
-            "Ana", "Carlos", "Beatriz", "Paulo", "Fernanda", "Lucas", "Juliana" };
-        List<string> cpfs = new List<string> { "12345678900", "09876543211", 
-            "11223344556", "66778899000", "11122233344", "55566677788", "99988877766", "44455566677", "33322211100", "00011122233" };
-        List<string> descricoesTipoConta = new List<string> { "Conta Corrente", "Conta Poupança", "Conta Salário" };
-        List<string> tiposMovimentacao = new List<string> { "Depósito", "Saque", "Transferência" };
-        List<string> fatorRisco = new List<string> { "Baixo", "Médio", "Alto" };
-
-        Random random = new Random();
-
-        // Gerar valores aleatórios
-        string nomeAleatorio = nomes[random.Next(nomes.Count)];
-        string cpfAleatorio = cpfs[random.Next(cpfs.Count)];
-        string descricaoTipoContaAleatoria = descricoesTipoConta[random.Next(descricoesTipoConta.Count)];
-        string tipoMovimentacaoAleatoria = tiposMovimentacao[random.Next(tiposMovimentacao.Count)];
-        double saldoAleatorio = random.Next(0, 10000);
-        double limiteNegativoAleatorio = random.Next(0, 1000);
-        double valorMovimentacaoAleatoria = random.Next(0, 5000);
-        string fatorRiscoAleatorio = fatorRisco[random.Next(fatorRisco.Count)];
-
-        // Criar objeto Cliente
-        var cliente = new Cliente
+        public Cliente GerarClienteAleatorio()
         {
-            Id = random.Next(1, 1000),
-            FatorRisco = fatorRiscoAleatorio,
-            RendaMensal = random.Next(1000, 10000).ToString(),
-            Pessoa = new Pessoa
+            // Listas de valores predefinidos
+            List<string> nomes = new List<string> { "João", "Maria", "Pedro", "Ana", "Carlos", "Beatriz", "Paulo", "Fernanda", "Lucas", "Juliana" };
+            List<string> cpfs = new List<string> { "12345678900", "09876543211", "11223344556", "66778899000", "11122233344", "55566677788", "99988877766", "44455566677", "33322211100", "00011122233" };
+            List<string> fatorRisco = new List<string> { "Baixo", "Médio", "Alto" };
+
+            Random random = new Random();
+
+            // Gerar valores aleatórios para o cliente
+            string nomeAleatorio = nomes[random.Next(nomes.Count)];
+            string cpfAleatorio = cpfs[random.Next(cpfs.Count)];
+            string fatorRiscoAleatorio = fatorRisco[random.Next(fatorRisco.Count)];
+
+            // Criar objeto Cliente
+            var cliente = new Cliente
             {
                 Id = random.Next(1, 1000),
-                Nome = nomeAleatorio,
-                Cpf = cpfAleatorio
-            },
-            Contas = new List<Conta>
-            {
-                new Conta
+                FatorRisco = fatorRiscoAleatorio,
+                RendaMensal = random.Next(1000, 10000).ToString(),
+                Pessoa = new Pessoa
                 {
                     Id = random.Next(1, 1000),
-                    Saldo = saldoAleatorio,
-                    LimiteNegativo = limiteNegativoAleatorio,
-                    TipoConta = new TipoConta
+                    Nome = nomeAleatorio,
+                    Cpf = cpfAleatorio
+                },
+                Contas = new List<Conta>
+                {
+                    GerarContaAleatoria(random)
+                }
+            };
+            return cliente;
+        }
+
+        public Conta GerarContaAleatoria(Random random)
+        {
+            // Listas de valores predefinidos
+            List<string> descricoesTipoConta = new List<string> { "Conta Corrente", "Conta Poupança", "Conta Salário" };
+            List<string> tiposMovimentacao = new List<string> { "Depósito", "Saque", "Transferência" };
+
+            // Gerar valores aleatórios para a conta
+            string descricaoTipoContaAleatoria = descricoesTipoConta[random.Next(descricoesTipoConta.Count)];
+            string tipoMovimentacaoAleatoria = tiposMovimentacao[random.Next(tiposMovimentacao.Count)];
+            double saldoAleatorio = random.Next(0, 10000);
+            double limiteNegativoAleatorio = random.Next(0, 1000);
+            double valorMovimentacaoAleatoria = random.Next(0, 5000);
+
+            // Criar objeto Conta
+            var conta = new Conta
+            {
+                Id = random.Next(1, 1000),
+                Saldo = saldoAleatorio,
+                LimiteNegativo = limiteNegativoAleatorio,
+                TipoConta = new TipoConta
+                {
+                    Id = random.Next(1, 10),
+                    Descricao = descricaoTipoContaAleatoria
+                },
+                ClienteId = random.Next(1, 1000),
+                MovimentacoesConta = new List<MovimentacaoConta>
+                {
+                    new MovimentacaoConta
                     {
-                        Id = random.Next(1, 10),
-                        Descricao = descricaoTipoContaAleatoria
-                    },
-                    ClienteId = random.Next(1, 1000),
-                    MovimentacoesConta = new List<MovimentacaoConta>
+                        Id = random.Next(1, 1000),
+                        DataMovimentacao = DateTime.Now,
+                        Valor = valorMovimentacaoAleatoria.ToString(),
+                        TipoMovimentacao = tipoMovimentacaoAleatoria                        
+                    }
+                },
+
+                Reservas = new List<Reserva>
+                {
+                    GerarReservaAleatoria(random)
+                }
+            };
+            return conta;
+        }
+
+        public Reserva GerarReservaAleatoria(Random random)
+        {
+            // Listas de valores predefinidos
+            List<string> reservas = new List<string> { "Reserva 1", "Reserva 2", "Reserva 3" };
+            List<string> tiposMovimentacao = new List<string> { "Depósito", "Saque", "Transferência" };
+
+            // Gerar valores aleatórios para a reserva
+            string reservaAleatoria = reservas[random.Next(reservas.Count)];
+            string tipoMovimentacaoAleatoria = tiposMovimentacao[random.Next(tiposMovimentacao.Count)];
+            double saldoAleatorio = random.Next(0, 10000);
+            double taxaAleatoria = random.Next(0, 1000);
+            double valorMovimentacaoAleatoria = random.Next(0, 5000);
+
+            // Criar objeto Reserva
+            var reserva = new Reserva
+            {
+                Id = random.Next(1, 1000),
+                Saldo = saldoAleatorio,
+                Taxa = taxaAleatoria,
+                ReservaCol = reservaAleatoria,
+                ContaId = random.Next(1, 1000),
+                MovimentacoesReserva = new List<MovimentacaoReserva>
+                {
+                    new MovimentacaoReserva
                     {
-                        new MovimentacaoConta
-                        {
-                            Id = random.Next(1, 1000),
-                            DataMovimentacao = DateTime.Now,
-                            Valor = valorMovimentacaoAleatoria.ToString(),
-                            TipoMovimentacao = tipoMovimentacaoAleatoria
-                        }
+                        Id = random.Next(1, 1000),
+                        DataMovimentacao = DateTime.Now,
+                        Valor = valorMovimentacaoAleatoria,
+                        TipoMovimentacao = tipoMovimentacaoAleatoria
                     }
                 }
-            }
-        };
-        return cliente;
-    }
+            };
+            return reserva;
+        }
+
+        public CartaoCredito GerarCartaoCreditoAleatorio(Random random)
+        {
+            // Listas de valores predefinidos
+            List<string> categoriasCartao = new List<string> { "Gold", "Platinum", "Black", "Silver", "Standard", "Enterprise" };
+
+            // Gerar valores aleatórios para o cartão de crédito
+            string categoriaCartaoAleatoria = categoriasCartao[random.Next(categoriasCartao.Count)];
+            string dataFechamentoAleatoria = random.Next(1, 28).ToString();
+            double limiteCreditoAleatorio = random.Next(0, 10000);
+
+            // Criar objeto CartaoCredito
+            var cartaoCredito = new CartaoCredito
+            {
+                Id = random.Next(1, 1000),
+                DataFechamento = dataFechamentoAleatoria,
+                ContaId = random.Next(1, 1000),
+                CategoriaCartao = new CategoriaCartao
+                {
+                    Id = random.Next(1, 10),
+                    Descricao = categoriaCartaoAleatoria
+                },
+                LimiteCredito = limiteCreditoAleatorio
+            };
+            return cartaoCredito;
+        }
 
         public void RunTests()
         {
-            //Gera um nome de pessoa aleatoriamente            
-
-            // var conta = new Conta
-            // {
-            //     Id = 1,
-            //     Saldo = 1000,
-            //     LimiteNegativo = 100,
-            //     TipoConta = new TipoConta
-            //     {
-            //         Id = 1,
-            //         Descricao = "Conta Corrente"
-            //     },
-            //     ClienteId = 1,
-            //     MovimentacoesConta = new List<MovimentacaoConta>
-            //     {
-            //         new MovimentacaoConta
-            //         {
-            //             Id = 1,
-            //             DataMovimentacao = DateTime.Now,
-            //             Valor = "0",
-            //             TipoMovimentacao = "Depósito"
-            //         }
-            //     }                                
-
-            // };
-
-            // var cliente = new Cliente
-            // {
-            //     Id = 1,
-            //     Pessoa = new Pessoa { Id = 1, Nome = "Fulano", Cpf = "12345678900" },
-            //     FatorRisco = "Médio",
-            //     RendaMensal = "5000",
-            //     Contas = new List<Conta> { conta }
-            // };
 
             var cliente = GerarClienteAleatorio();
 
@@ -137,28 +178,7 @@ public Cliente GerarClienteAleatorio()
             var clienteInserido = clienteDAO.GetByPessoaId(idPessoa);  //OK
 
             //Adicionar uma nova conta a um cliente existente
-            var conta2 = new Conta
-            {
-                Id = 2,
-                Saldo = 2000,
-                LimiteNegativo = 200,
-                TipoConta = new TipoConta
-                {
-                    Id = 2,
-                    Descricao = "Conta Poupança"
-                },
-                ClienteId = clienteInserido.Id,
-                MovimentacoesConta = new List<MovimentacaoConta>
-                {
-                    new MovimentacaoConta
-                    {
-                        Id = 1,
-                        DataMovimentacao = DateTime.Now,
-                        Valor = "0",
-                        TipoMovimentacao = "Depósito"
-                    }
-                }
-            };
+            var conta2 = GerarContaAleatoria(new Random());
 
             clienteInserido.Contas.Add(conta2);
 
@@ -170,25 +190,7 @@ public Cliente GerarClienteAleatorio()
             clienteDAO.Update(clienteInserido);
 
             //Cria reservas para conta 1
-            var reserva1 = new Reserva
-            {
-                Id = 1,
-                Saldo = 100,
-                Taxa = 0.1,
-                ReservaCol = "Reserva 1",
-                ContaId = clienteInserido.Contas[0].Id,
-                MovimentacoesReserva = new List<MovimentacaoReserva>
-                {
-                    new MovimentacaoReserva
-                    {
-                        Id = 1,
-                        DataMovimentacao = DateTime.Now,
-                        Valor = 0,
-                        TipoMovimentacao = "Reserva",
-                        ReservaId = 1
-                    }
-                }
-            };
+            var reserva1 = GerarReservaAleatoria(new Random());
 
             //Insere a reserva para conta 1 // OK
             reservaDAO.Insert(reserva1);
@@ -198,14 +200,7 @@ public Cliente GerarClienteAleatorio()
             clienteInserido = clienteDAO.GetByPessoaId(idPessoa);
 
             //Cria um cartao de credito
-             var cartaoCredito = new CartaoCredito
-             {
-                 Id = 1,
-                 DataFechamento = "10",
-                 ContaId = clienteInserido.Contas[0].Id,
-                 CategoriaCartao = new CategoriaCartao { Id = 1, Descricao = "Gold" },
-                 LimiteCredito = 1000
-             };
+             var cartaoCredito = GerarCartaoCreditoAleatorio(new Random());
 
             //Insere o cartao de credito
             cartaoCreditoDAO.Insert(cartaoCredito);
